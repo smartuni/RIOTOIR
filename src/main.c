88c0e996dbc4ca17e9b9363057701aaba601d520
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2018-10-26 10:18:02 +0200

    pulse detector

--- src/main.c
+++ src/main.c
@@ -2,11 +2,49 @@
 #include <string.h>
 
 #include "shell.h"
+#include "led.h"
+#include "xtimer.h"
+#include "thread.h"
+#include "periph/gpio.h"
+#include "tsrb.h"
+
+static unsigned int isr_counter = 0;
+
+gpio_t transistor_pin = GPIO_PIN(PA, 13);
+
+void *thread_handler(void *arg)
+{
+    (void)arg;
+
+    while(true) {
+        xtimer_sleep(1);
+
+        printf("%ld: %d\n", xtimer_now().ticks32, isr_counter);
+        printf("%d\n", gpio_read(transistor_pin));
+        LED0_TOGGLE;
+    }
+    return NULL;
+}
+
+void isr(void *arg) {
+    (void)arg;
+    const uint32_t timestamp = xtimer_now().ticks32;
+
+    ++isr_counter;
+}
 
 int main(void)
 {
     puts("This is the RIOTOIR project\n");
     printf("This application runs on %s\n", RIOT_BOARD);
+
+    thread_create(stack, sizeof(stack),
+                  THREAD_PRIORITY_MAIN - 1,
+                  THREAD_CREATE_STACKTEST,
+                  thread_handler, NULL,
+                  "time_printer");
+
+    gpio_init_int(transistor_pin, GPIO_IN, GPIO_RISING, isr, NULL);
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
