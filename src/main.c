98b7d9cc9e06816b8ca96d49dc822469bb3905fb
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2018-11-20 15:19:26 +0100

    integrate sender and receiver

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 #include "periph/gpio.h"
 #include "shell.h"
 
-#include "ir.h"
+#include "receive_ir.h"
+#include "send_ir.h"
+
 #define BUFFERSIZE 32
 
 
@@ -13,16 +15,33 @@
 gpio_t transistor_pin = GPIO_PIN(PA, 13);
 
 
+int send_msg_handler(int argc, char **argv) {
+    if (argc != 1) {
+        puts("Illegal argument count");
+        return 1;
+    }
+
+    messageSend(0, 0, (uint8_t *) argv[0], strlen(argv[0]));
+}
+
+static const shell_command_t commands[] = {
+        {"sendmsg", "sends message via IR", send_msg_handler},
+        { NULL, NULL, NULL }
+};
+
+
+
 int main(void)
 {
     puts("This is the RIOTOIR project\n");
     printf("This application runs on %s\n", RIOT_BOARD);
 
     setup_ir_recv(transistor_pin);
+    setup_ir_send();
 
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    shell_run(commands, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
