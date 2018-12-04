b46120d83d3f2a209fbaa0910a298920ea355be2
Hauke Goldhammer <hauke.goldhammer@haw-hamburg.de>
2018-12-04 14:07:11 +0100

    isr init pd

--- src/smart-meter/main.c
+++ src/smart-meter/main.c
@@ -9,6 +9,7 @@
 #include "net/sock/udp.h"
 #include "net/sntp.h"
 #include "tsrb.h"
+#include "fmt.h"
 
 static unsigned int isr_counter = 0;
 
@@ -16,53 +17,71 @@ static char timestamps[128];
 
 static char stack[THREAD_STACKSIZE_MAIN];
 
-static char ntp_stack[THREAD_STACKSIZE_MAIN];
+//static char ntp_stack[THREAD_STACKSIZE_MAIN];
 
-gpio_t transistor_pin = GPIO_PIN(PA, 13);
+gpio_t transistor_pin = GPIO_PIN(PA, 18);
 
 static tsrb_t queue = TSRB_INIT(timestamps);
 
 void *thread_handler(void *arg) {
     (void) arg;
-    sock_udp_ep_t server = SOCK_IPV4_EP_ANY;
-    uint64_t msg_payload[20];
+    //sock_udp_ep_t server = SOCK_IPV4_EP_ANY;
+    uint64_t msg_payload[40];
+
 
     // send a timestamp for every use of 1Wh to cloud
-    server.addr.ipv4[0] = 127;
-    server.addr.ipv4[1] = 0;
-    server.addr.ipv4[2] = 0;
-    server.addr.ipv4[3] = 1;
+    /*ipv4_addr_t server_addr;
+    ipv4_addr_from_str(&server_addr, "127.0.0.1");
+    server.addr.ipv4_u32 = server_addr.u32.u32;
     server.port = 7;
 
     sock_udp_t sock;
-    if (sock_udp_create(&sock, NULL, &server, 0) < 0) {
+    if (sock_udp_create(&sock, NULL, NULL, 0) < 0) {
+        perror("no udp socket created!");
         return NULL;
     }
+     */
 
     while (true) {
         xtimer_sleep(5);
         int count = tsrb_get(&queue,(char *) &msg_payload, sizeof(msg_payload));
-        sock_udp_send(&sock, (void *) &msg_payload, sizeof(uint64_t) * count, NULL);
+        if(count > 0) {
+            /*if (sock_udp_send(&sock, (void *) &msg_payload, sizeof(uint64_t) * count, &server) < 0) {
+                perror("no timestamps send per udp");
+            }*/
+            // Testing
+            for (int i = 0; i < count / 8; ++i) {
+                char print[22];
+                print[21] = '\0';
+                fmt_u64_dec((char*)&print, msg_payload[i]);
+                printf("used 1Wh at %s\n", print);
+            }
+        } else {
+            printf("nix in der queue");
+        }
     }
     return NULL;
 }
 
-void *ntp_thread_handler(void *arg) {
+/*void *ntp_thread_handler(void *arg) {
     (void) arg;
     sock_udp_ep_t server = SOCK_IPV4_EP_ANY;
 
     // nutzt den HAW NPT Server 141.22.13.9
-    server.addr.ipv4[0] = 141;
-    server.addr.ipv4[1] = 22;
-    server.addr.ipv4[2] = 13;
-    server.addr.ipv4[3] = 9;
+    ipv4_addr_t sntp_server_addr;
+    ipv4_addr_from_str(&sntp_server_addr, "141.22.13.9");
+    server.addr.ipv4_u32 = sntp_server_addr.u32.u32;
     server.port = 123;
     while (true) {
-        sntp_sync(&server, 1000);
-        xtimer_sleep(3600);
+        int send_err = sntp_sync(&server, 10);
+        if(send_err < 0) {
+            perror("unix time sync error");
+        }
+        xtimer_sleep(1);
+        printf("err:%d time: %llu\n", send_err, sntp_get_unix_usec());
     }
 
-}
+}*/
 
 void isr(void *arg) {
     (void) arg;
@@ -74,7 +93,6 @@ void isr(void *arg) {
         tmp = sntp_get_unix_usec();
         tsrb_add(&queue, (char *) &tmp , sizeof(tmp));
         isr_counter = 0;
-
     }
 }
 
@@ -82,13 +100,15 @@ int main(void) {
 
     puts("This is the RIOTOIR: smart meter\n");
 
-    thread_create(ntp_stack, sizeof(ntp_stack), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, ntp_thread_handler,
-                  NULL, "ntp_sync");
+    //thread_create(ntp_stack, sizeof(ntp_stack), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, ntp_thread_handler,
+    //              NULL, "ntp_sync");
 
     thread_create(stack, sizeof(stack), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, thread_handler, NULL,
-                  "time_printer");
+                  "Wh consume printer");
 
-    gpio_init_int(transistor_pin, GPIO_IN, GPIO_RISING, isr, NULL);
+    if(gpio_init_int(transistor_pin, GPIO_IN_PD, GPIO_RISING, isr, NULL) < 0){
+        puts("Interrupt init Fehler");
+    }
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
